6398d3f20dadecd2f9e5391021e478daad244fe2
Mariana Gularte <114247639+Mariana-gularte88@users.noreply.github.com>
2023-05-26 19:51:32 -0300

    Update CartWidget.jsx

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,14 +1,18 @@
-
+import carrito from '../assets2/carrito.svg';
+import { useContext } from 'react';
+import { CartContext } from '../Context/CartContext';
+import { Link } from 'react-router-dom';
 
 const CartWidget = () => {
+  const { totalQuantity } = useContext(CartContext)
+
   return (
-    <>
-    <span className="material-symbols-outlined">
-        shopping_cart
-       
-    </span>
-    <span>0</span>
-    </>
+    <Link to='/cart' style={{display: totalQuantity > 0 ? 'block' : 'none'}}>
+      <img src={carrito} alt="carrito-compras" />
+      
+      {totalQuantity}
+      
+    </Link>
   )
 }
 
